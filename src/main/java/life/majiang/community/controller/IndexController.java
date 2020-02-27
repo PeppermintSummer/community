30e662df29e84c46034de85b3dad898db2cfad97
PeppermintSummer <528129929@qq.com>
2020-02-27 14:39:05 +0800

    add pages

--- src/main/java/life/majiang/community/controller/IndexController.java
+++ src/main/java/life/majiang/community/controller/IndexController.java
@@ -1,10 +1,9 @@
 package life.majiang.community.controller;
 
 
+import life.majiang.community.dto.PaginationDTO;
 import life.majiang.community.dto.QuestionDTO;
-import life.majiang.community.mapper.QuestionMapper;
 import life.majiang.community.mapper.UserMapper;
-import life.majiang.community.model.Question;
 import life.majiang.community.model.User;
 import life.majiang.community.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +11,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
@@ -29,7 +29,10 @@ public class IndexController {
 
     @GetMapping("/")   //访问首页
     public String index(HttpServletRequest request,
-                        Model model){
+                        Model model,
+                        @RequestParam(name = "page",defaultValue = "1") Integer page,
+                        @RequestParam(name = "size",defaultValue = "2") Integer size
+                        ){
         Cookie[] cookies = request.getCookies();//request可以去设置cookie
         if (cookies!=null && cookies.length!=0)
             for (Cookie cookie : cookies) {
@@ -42,8 +45,8 @@ public class IndexController {
                     break;
                 }
             }
-        List<QuestionDTO> questionList=questionService.list();//带有用户信息和question信息
-        model.addAttribute("questions",questionList);
+        PaginationDTO pagination=questionService.list(page,size);//带有用户信息和question信息
+        model.addAttribute("pagination",pagination);
         return "index";
     }
 //    public String hello(@RequestParam(name="name") String name, Model model){
---
+++ src/main/java/life/majiang/community/dto/PaginationDTO.java
@@ -0,0 +1,70 @@
+package life.majiang.community.dto;
+
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+public class PaginationDTO {  //页码需要展示、封装
+    private List<QuestionDTO> questions;
+    private boolean showPrevious;
+    private boolean showFirstPage;
+    private boolean showNext;
+    private boolean showEndPage;
+    private Integer page;       //数组当前页
+    private List<Integer> pages=new ArrayList<>();//数组表示当前多少页
+    private Integer totalPage;
+
+    public void setPagination(Integer totalCount, Integer page, Integer size) {
+        if(totalCount % size==0){
+            totalPage=totalCount/size;
+        }
+        else totalPage=totalCount/size+1;
+
+        if (page<1){
+            page=1;
+        }
+        if(page>totalPage){
+            page=totalPage;
+        }
+        this.page=page;
+        //分页逻辑
+        pages.add(page);//把当前页加进去
+        for (int i = 1; i <= 3; i++) {
+            if (page-i>0){
+                pages.add(0,page-i);
+            }
+            if(page+i<=totalPage){
+                pages.add(page+i);
+            }
+        }
+        //是否展示上一页
+        if (page==1){
+            showPrevious=false;
+        }else{
+            showPrevious=true;
+        }
+
+        if(page==totalPage)
+        {
+            showNext=false;
+        }
+        else{
+            showNext=true;
+        }
+
+        //展示第1页
+        if (pages.contains(1)){
+            showFirstPage= false;
+        }else{
+            showFirstPage=true;
+        }
+        //展示最后一页
+        if (pages.contains(totalPage)){
+            showEndPage=false;
+        }else{
+            showEndPage=true;
+        }
+    }
+}
