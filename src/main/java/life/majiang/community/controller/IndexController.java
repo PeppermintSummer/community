e64938fcad0817ac700a2003e0c9f614af35091c
PeppermintSummer <528129929@qq.com>
2020-03-19 13:20:25 +0800

    fix some log_in problems up

--- src/main/java/life/majiang/community/controller/IndexController.java
+++ src/main/java/life/majiang/community/controller/IndexController.java
@@ -2,9 +2,6 @@ package life.majiang.community.controller;
 
 
 import life.majiang.community.dto.PaginationDTO;
-import life.majiang.community.dto.QuestionDTO;
-import life.majiang.community.mapper.UserMapper;
-import life.majiang.community.model.User;
 import life.majiang.community.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -12,10 +9,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
-import java.util.List;
 
 @Controller
 @Component
@@ -31,15 +24,17 @@ public class IndexController {
     public String index(//HttpServletRequest request,
                         Model model,
                         @RequestParam(name = "page",defaultValue = "1") Integer page,
-                        @RequestParam(name = "size",defaultValue = "6") Integer size
+                        @RequestParam(name = "size",defaultValue = "6") Integer size,
+                        @RequestParam(name = "search" ,required = false) String search
                         ){
 
-        PaginationDTO pagination=questionService.list(page,size);//带有用户信息和question信息
+        // 搜索采用正则匹配的方式,也可以ElasticSearch,dubbo,springcloud,
+        PaginationDTO pagination=questionService.list(search,page,size); //带有用户信息和question信息
         model.addAttribute("pagination",pagination);
         return "index";
     }
 //    public String hello(@RequestParam(name="name") String name, Model model){
 //        model.addAttribute("name" ,name);
-//        return "index";//自动去模板目录找
+//        return "index"; //自动去模板目录找
 //    }
 }
--- src/main/java/life/majiang/community/service/QuestionService.java
+++ src/main/java/life/majiang/community/service/QuestionService.java
@@ -2,6 +2,7 @@ package life.majiang.community.service;
 
 import life.majiang.community.dto.PaginationDTO;
 import life.majiang.community.dto.QuestionDTO;
+import life.majiang.community.dto.QuestionQueryDTO;
 import life.majiang.community.exception.CustomizeErrorCode;
 import life.majiang.community.exception.CustomizeException;
 import life.majiang.community.mapper.QuestionExtMapper;
@@ -31,14 +32,23 @@ public class QuestionService {
     @Autowired
     private QuestionExtMapper questionExtMapper;
 
-    public PaginationDTO list(Integer page, Integer size) {
+    public PaginationDTO list(String search,Integer page, Integer size) {
+        if (StringUtils.isNotBlank(search)){
+            String[] tags=StringUtils.split(search,  " ");
+            search=Arrays.stream(tags).collect(Collectors.joining("|"));
+        }
+
+
+
         PaginationDTO paginationDTO = new PaginationDTO();
         //Integer totalCount = questionMapper.count();//拿到所有的分页数
         //paginationDTO.setPagination(totalCount,page,size);
 
         Integer totalPage;
         //Integer totalCount = questionMapper.count();//拿到所有的分页数
-        Integer totalCount = (int)questionMapper.countByExample(new QuestionExample());//拿到所有的分页数
+        QuestionQueryDTO questionQueryDTO = new QuestionQueryDTO();
+        questionQueryDTO.setSearch(search);
+        Integer totalCount = questionExtMapper.countBySerach(questionQueryDTO);//拿到所有的分页数
 
         if(totalCount % size==0){
             totalPage=totalCount/size;
@@ -59,7 +69,10 @@ public class QuestionService {
 
         QuestionExample questionExample = new QuestionExample();
         questionExample.setOrderByClause("gmt_create desc");
-        List<Question> questions = questionMapper.selectByExampleWithRowbounds(questionExample, new RowBounds(offset, size));
+        //List<Question> questions = questionMapper.selectByExampleWithBLOBsWithRowbounds(questionExample, new RowBounds(offset, size));
+        questionQueryDTO.setSize(size);
+        questionQueryDTO.setPage(offset);
+        List<Question> questions = questionExtMapper.selectBySearch(questionQueryDTO);
         List<QuestionDTO> questionDTOList=new ArrayList<>();
 
         for (Question question : questions) {
